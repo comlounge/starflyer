e2eda4fd1d221a8dd17552f0f0a219fb37ce04bb
Carsten Rebbien <cr@comlounge.net>
2011-05-03 10:43:55 +0200

    removed old static handler imports

--- starflyer/__init__.py
+++ starflyer/__init__.py
@@ -1,4 +1,4 @@
 from app import Application
-from handler import Handler, StaticHandler, StaticHandlerFactory
+from handler import Handler
 from decorators import ashtml, asjson
 from settings import AttributeMapper
