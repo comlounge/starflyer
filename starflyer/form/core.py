18e14deac2a299927b36187265cc5ae03cf8565a
Christian Scholz <mrtopf@gmail.com>
2011-05-15 01:19:30 +0200

    - when calling a widget from a template to render it you can now pass additional attributes to be passed either to the widget itself or the surrounding field - the value will now be passed to the widget correctly

--- starflyer/form/core.py
+++ starflyer/form/core.py
@@ -8,12 +8,24 @@ class RenderContext(object):
         self.form = form
         self.widget = widget
 
-    def __call__(self):
-        """render the widget"""
-        tag = self.widget.render(self)
-        tmpl = self.form.tmpl_env.get_template("widget.html")
+    def __call__(self, widget_attrs={}, field_attrs={}, template=None):
+        """render the widget. This will be called form within
+        the template. You can pass additional parameters for both 
+        the widget (e.g. the actual input tag) in ``widget_attrs`` 
+        and the field (the surrounding structure with label etc.)
+        in ``field_attrs``.
+
+        If you wish you can also pass a new template name for the field
+        to be used in ``template``. It defaults to the name define in 
+        ``widget_template`` in the form class. 
+        
+        """
+        if template is None:
+            template = self.form.widget_template
+        tag = self.widget.render(self, **widget_attrs)
+        tmpl = self.form.template_env.get_template(template)
         error = self.form.errors.get(self.widget.name, None)
-        return tmpl.render(data=self.widget, tag=tag, form = self.form, error=error)
+        return tmpl.render(data=self.widget, tag=tag, form = self.form, error=error, **field_attrs)
 
 class Widget(object):
     """a field base class which emits widgets"""
@@ -46,20 +58,45 @@ class Widget(object):
         self.required = required
         self.additional = kwargs
 
-    def render(self, context = None):
+    def get_value(self, form):
+        """return the value to be displayed inside the widget. This will either
+        come from the ``defaults`` attribute of the form or it`s ``values`` dict.
+        The former will usually be populated with data from an database record and
+        the latter will be populated from a previous form input, e.g. on an error
+        condition on a different widget."""
+
+        _no_value = object()
+
+        n = self.name
+        value = form.values.get(n, _no_value)
+        if value is _no_value:
+            value = form.default.get(n, u'')
+        return value
+        
+    def render(self, render_context = None, add_class="", **kwargs):
         """render this widget. We also pass in the ``RenderContext`` instance
         to be used in order to be able to access the ``Form`` instance and additional
-        runtime information contained within."""
+        runtime information contained within.
+
+        Additionally you can provide an ``add_class`` parameter which can contain
+        a class name to be added to the CSS classes for the input tag
+
+        Any additional keyword arguments passed will override the original attributes
+        used for rendering the tag
+
+        Note that this method will only input e.g. a input tag and not the
+        surrounding divs etc.
+        """
         attrs = {}
         for a in self.BASE_ATTRS+self.ATTRS:
             attrs[a] = getattr(self, a)
         attrs.update(self.additional)
-        attrs['class'] = attrs['css_class']
+        attrs.update(kwargs)
+        attrs['class'] = attrs['css_class']+" "+add_class
         del attrs["css_class"]
 
         # process the value to be displayed
-        if context.form.default.has_key(self.name):
-            attrs['value'] = context.form.default[self.name]
+        attrs['value'] = self.get_value(render_context.form)
 
         attrs = ['%s="%s"' %(a,werkzeug.escape(v, True)) for a,v in attrs.items()]
         attrs = " ".join(attrs)
@@ -74,19 +111,19 @@ class Form(object):
     """a form"""
 
     error_css_class = "error"
+    widget_template = "widget.html"
 
-    def __init__(self, tmpl_env, default = {}, values = {}, errors= {}, vocabs = {}):
+    def __init__(self, template_env, default = {}, values = {}, errors= {}, vocabs = {}):
         """initialize the form's widget. We pass in a ``tmpl_env`` which is a 
         ``jinja2.Environment`` and should contain a template called ``field.html``
         to be used for rendering a single field. Optionally you can pass in 
         ``vocabs`` which should contain the vocabularies to be used for select
         fields etc. ``default`` is a dictionary with which you can pass in data
         into the form as initial values."""
-        print errors
-        self.tmpl_env = tmpl_env
+        self.template_env = template_env
         self.vocabs = vocabs
-        self.default = default
-        self.values = values
+        self.default = default # this might come from an object
+        self.values = values # this might come from a form and overrides the defaults
         self.errors = errors
         self.data = {}
         for widget in self.widgets:
