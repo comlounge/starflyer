39c88c12c7235a7eaa9dc12df00602c263469526
Christian Scholz <cs@comlounge.net>
2013-05-02 17:00:58 +0200

    fixed setup for beta release

--- setup.py
+++ setup.py
@@ -1,32 +1,29 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '2.0a'
+version = '2.0b'
 
 setup(name='starflyer',
       version=version,
       description="Starflyer web framework",
-      long_description="""super\
+      long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='COM.lounge',
       author_email='info@comlounge.net',
       url='',
-      license='MIT',
+      license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
         "logbook",
         "werkzeug",
-        "blinker",
         "Paste",
         "PasteDeploy",
-        "pyyaml",
         "argparse",
         "jinja2",
-        "python-daemon",
       ],
       entry_points="""
       [console_scripts]
