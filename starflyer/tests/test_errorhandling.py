c9b2999bac67d1d1ba3bcbef03b03a58f8db043d
Christian Scholz <cs@comlounge.net>
2013-06-05 16:05:35 +0200

    added some exceptions to testing=True as in error handling we need to test the real thing with real exceptions

--- starflyer/tests/test_errorhandling.py
+++ starflyer/tests/test_errorhandling.py
@@ -6,6 +6,7 @@ def test_route_not_found(client):
 
 def test_broken_handler_with_debug(client):
     client.application.config.debug = True
+    client.application.config.testing = False # somewhat more real here
     # propagate_exception is True and thus we get the exception directly
     # which is useful in unit tests like this one
     pytest.raises(NameError, client.get, "/broken")
@@ -13,6 +14,7 @@ def test_broken_handler_with_debug(client):
 def test_broken_handler_no_propagate(client):
     """check if the right thing happens if we don't have an error handler defined"""
     client.application.config.debug = True
+    client.application.config.testing = False # somewhat more real here
     client.application.error_handlers = {}
     # this means to use the default error handler (InternalServerError)
     client.application.config.propagate_exceptions = False
@@ -22,12 +24,14 @@ def test_broken_handler_no_propagate(client):
 def test_broken_handler_propagate(client):
     """check if the right thing happens if we don't have an error handler defined"""
     client.application.config.debug = True
+    client.application.config.testing = False # somewhat more real here
     client.application.error_handlers = {}
     client.application.config.propagate_exceptions = True
     pytest.raises(NameError, client.get, "/broken")
 
 def test_error_handler(client):
     client.application.config.debug = False
+    client.application.config.testing = False # somewhat more real here
     resp = client.get("/broken")
     assert resp.status_code == 200
 
@@ -37,12 +41,14 @@ def test_error_handler(client):
 @pytest.mark.xfail()
 def test_strict_slashes_in_debug(client):
     client.application.config.debug = True
+    client.application.config.testing = False # somewhat more real here
     resp = client.post("/branch")
     assert resp.status_code == 301
     assert resp.headers['location'] == "http://localhost/branch/"
 
 def test_strict_slashes_no_debug(client):
     client.application.config.debug = False
+    client.application.config.testing = False # somewhat more real here
     # actually you should not post to that in the first place
     resp = client.post("/branch")
     assert resp.status_code == 301
